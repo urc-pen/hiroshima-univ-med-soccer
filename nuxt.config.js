7adbe92342fa9c02cb165886a7fb4c5b02895d41
Soma Urushidani <somaurushidani@SomanoMacBook-Air.local>
2026-06-17 18:36:07 +0900

    Add Google Search Console verification

--- nuxt.config.js
+++ nuxt.config.js
@@ -59,7 +59,11 @@ export default {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: pkg.description }
+      { hid: 'description', name: 'description', content: pkg.description },
+      {
+        name: 'google-site-verification',
+        content: 'ZzSK9xwE6oF2gR9u4PqNwKPwjEZ1fB-XmbYLwgFn2KI'
+      }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
